2bb0d3a65a5c8e58a6cfd411bca30933bd9296a6
drewsauce <drewruppel@gmail.com>
2016-10-06 16:47:41 -0700

    setup or permissions and roles

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function roles() {
+        return $this->belongsToMany('Roles', 'roles_permissions', 'permission_id', 'role_id');
+    }
+
+    public function users() {
+        return $this->belongsToMany('Users', 'id', 'id');
+    }
 }
--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * Get all permissions
+     *
+     * need to refactor this badly.  should be using eloquent relations to get the permissions
+     *
+     * @return mixed
+     */
+    public function permissions() {
+        return $this->belongsToMany('Permissions', 'roles_permissions', 'role_id', 'permission_id');
+    }
+
+    public function user() {
+        return $this->belongsTo('User', 'id', 'id');
+    }
 }
