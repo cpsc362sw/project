8cff825995940dbcdde74a76c106aa85474e5a59
druppel <druppel@goldencomm.com>
2016-10-12 13:42:30 -0700

    add proper migration for default for those of us that have it downloaded already

--- database/migrations/2016_10_12_204113_add_user_role_id_default_value.php
+++ database/migrations/2016_10_12_204113_add_user_role_id_default_value.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserRoleColumn extends Migration
+class AddUserRoleIdDefaultValue extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddUserRoleColumn extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->integer('role_id')->after('email');
+            $table->integer('role_id')->after('email')->default(2);
         });
     }
 
