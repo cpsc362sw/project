3053ba50759ad06ee76dec59f4c392c7ca615498
drewsauce <drewruppel@gmail.com>
2016-10-13 10:49:11 -0700

    quick changes to admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,16 +16,36 @@
 
                     <div class="panel-body">
                         <a href="{{ url('/admin/users') }}">
-                            <div class="col-md-6 user"></div>
+                            <div class="col-md-6 user">
+                                <label>
+                                    <i class="fa fa-users" aria-hidden="true"></i>
+                                    Users
+                                </label>
+                            </div>
                         </a>
                         <a href="{{ url('/admin/calendar') }}">
-                            <div class="col-md-6 calendar"></div>
+                            <div class="col-md-6 calendar">
+                                <label>
+                                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                                    Calendar
+                                </label>
+                            </div>
                         </a>
                         <a href="{{ url('/admin/payroll') }}">
-                            <div class="col-md-6 payroll"></div>
+                            <div class="col-md-6 payroll">
+                                <label>
+                                    <i class="fa fa-money" aria-hidden="true"></i>
+                                    Payroll
+                                </label>
+                            </div>
                         </a>
                         <a href="{{ url('/admin/timeclock') }}">
-                            <div class="col-md-6 timeclock"></div>
+                            <div class="col-md-6 timeclock">
+                                <label>
+                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                    Timeclock
+                                </label>
+                            </div>
                         </a>
                     </div>
                 </div>
