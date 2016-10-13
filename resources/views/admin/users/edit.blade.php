d0c1785cc2cfeed90ba9773a625ccc842c598a9f
drewsauce <drewruppel@gmail.com>
2016-10-13 15:05:40 -0700

    tweak routes for getting a single user

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: drupp
- * Date: 10/13/2016
- * Time: 2:50 PM
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><b>Administrative</b> Dashboard
+                        <i class="fa fa-angle-right" aria-hidden="true"></i> Users
+                        <i class="fa fa-angle-right" aria-hidden="true"></i> Edit
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
