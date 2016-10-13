bbc508cf15bf0a09d58ff7515fe335b75c772ff8
drewsauce <drewruppel@gmail.com>
2016-10-13 11:17:37 -0700

    add users landing page to routes.

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,28 @@ Route::group(['middleware' => ['auth']], function() {
         'uses' => 'HomeController@index',
     ]);
     Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function() {
+        # dashboard landing page
         Route::get('/', [
             'as' => 'admin',
             'uses' => 'AdminController@index'
         ]);
+        #  users landing page
+        Route::get('/users', [
+            'as' => 'admin.users',
+            'uses' => 'AdminController@getUsers'
+        ]);
+
         Route::get('/users/edit/{id}', [
             'as' => 'postuser',
             'uses' => 'AdminController@postUser'
         ]);
+
+        # calendar landing page
+
+        # timeclock landing page
+
+        # payroll landing page
+
 
     });
 
