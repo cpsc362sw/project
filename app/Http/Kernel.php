298222a7961dc4d338c351d30267e2e33a7b7dff
drewsauce <drewruppel@gmail.com>
2016-11-12 13:04:53 -0800

    register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\isAdmin::class,
+        'manager' => \App\Http\Middleware\isManager::class,
         'user' => \App\Http\Middleware\isUser::class,
     ];
 }
