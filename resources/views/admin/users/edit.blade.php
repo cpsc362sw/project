5ac428e4346282ac799f54503225e3642b056c7f
Eric Parra <parra@csu.fullerton.edu>
2016-11-17 01:33:40 -0800

    Made page layout/style consistent with others

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,23 +8,43 @@
                     <div class="panel-heading"><b>Administrative</b> Dashboard
                         <i class="fa fa-angle-right" aria-hidden="true"></i> Users
                         <i class="fa fa-angle-right" aria-hidden="true"></i> Edit
-                        <br />
-                        <table class="table table-striped table-hover">
-                            <form method="post" action="">
-                                <input type="text" name="name" value="{{ $user->name }}" />
-                                <input type="email" name="email" value="{{ $user->email }}" />
-                                <select name="role_id">
-                                    @foreach($role as $role_id => $role_name)
-                                        @if ($role_id == $user->role_id)
-                                            <option value="{{ $role_id }}" selected>{{ ucfirst($role_name) }}</option>
-                                        @else
-                                            <option value="{{ $role_id }}">{{ ucfirst($role_name) }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                                <button type="submit">Edit User</button>
-                            </form>
-                        </table>
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="post" action="">
+                            <div class="form-group">
+                                <label for="name" class="col-md-4 control-label">Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" type="text" name="name" value="{{ $user->name }}" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email" class="col-md-4 control-label">E-mail Address</label>
+                                <div class="col-md-6">
+                                    <input type="email" name="email" value="{{ $user->email }}" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="role_id" class="col-md-4 control-label">Role</label>
+                                <div class="col-md-6">
+                                    <select name="role_id">
+                                        @foreach($role as $role_id => $role_name)
+                                            @if ($role_id == $user->role_id)
+                                                <option value="{{ $role_id }}" selected>{{ ucfirst($role_name) }}</option>
+                                            @else
+                                                <option value="{{ $role_id }}">{{ ucfirst($role_name) }}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Save Changes</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
