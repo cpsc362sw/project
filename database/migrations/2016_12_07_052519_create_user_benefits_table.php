2a5e2d44780a034810f7faecc16a877de8e836de
Payaam Emami <payaamemami@gmail.com>
2016-12-07 22:52:23 -0800

    continued user benefits, added database migration along with controller logic, still need to populate view on user dashboard. will continue tomorrow

--- database/migrations/2016_12_07_052519_create_user_benefits_table.php
+++ database/migrations/2016_12_07_052519_create_user_benefits_table.php
@@ -15,6 +15,24 @@ class CreateUserBenefitsTable extends Migration
     {
     	Schema::create('benefits', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->string('health');
+            $table->string('vision');
+            $table->string('dental');
+            $table->string('life');
+            $table->string('retirement');
+            $table->string('first_1');
+            $table->string('last_1');
+            $table->string('relation_1');
+            $table->string('first_2');
+            $table->string('last_2');
+            $table->string('relation_2');
+            $table->string('first_3');
+            $table->string('last_3');
+            $table->string('relation_3');
+            $table->string('first_4');
+            $table->string('last_4');
+            $table->string('relation_4');
     	});
     }
 
