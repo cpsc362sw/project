9f05fc36c5f76cbe47b086643a1c08fda1e2efcb
drewsauce <drewruppel@gmail.com>
2016-12-08 20:12:12 -0800

    im sorry eric

--- resources/views/user/timeclock/index.blade.php
+++ resources/views/user/timeclock/index.blade.php
@@ -41,35 +41,35 @@
                     <div class="panel-heading"><b>User</b> Dashboard <i class="fa fa-angle-right" aria-hidden="true"></i> Timeclock <i class="fa fa-angle-right" aria-hidden="true"></i> Edit</div>
                     <div class="panel-body" style="font-size: 120%;">
                         @foreach($entries as $day => $group)
-                        <div class="time-edit-box">
-                            <label class="large-title">Day: {{ $day }}</label>
-                            <table>
-                            @for ($i = 0; $i < 4; $i++)
-                                @php (isset($group[$i]->id) ? $entry = $group[$i] : $entry = NULL)
-                                <tr style="line-height: 3em;">
-                                    <td style="width:150px; font-weight:600;">
-                                        <i class="fa fa-circle-thin" aria-hidden="true" style="font-size: 50%;"></i>
-&nbsp;&nbsp;                                      {{ ucwords(str_replace('_', ' ', $entryType[$i])) }}:
-                                    </td>
-                                    <td style="width:500px;">
-                                        <form method="post" action="{{ url('/user/timeclock/edit') }}">
-                                            {{ csrf_field() }}
-                                            <input type="hidden" name="id" value="{{ $entry->id or NULL }}" />
-                                            <input type="hidden" name="type" value="{{ $entryType[$i] }}" />
-                                            <input type="hidden" name="date" value="{{ $day }}" />
-                                            <input value="{{ isset($entry->time) ? date('H:i:s', strtotime($entry->time)) : '00:00:00' }}" style="border:none; width:75px;" disabled="disabled" />
-                                            <input name="time" value="{{ isset($entry->time) ? date('H:i:s', strtotime($entry->time)) : '00:00:00' }}" />
-                                            <button class="btn btn-block" style="width:150px;display:inline;">Submit Change&nbsp;<i class="fa fa-pencil"></i></button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endfor
+                            <div class="time-edit-box">
+                                <label class="large-title">Day: {{ $day }}</label>
+                                <table>
+                                    @for ($i = 0; $i < 4; $i++)
+                                        @php (isset($group[$i]->id) ? $entry = $group[$i] : $entry = NULL)
+                                        <tr style="line-height: 3em;">
+                                            <td style="width:150px; font-weight:600;">
+                                                <i class="fa fa-circle-thin" aria-hidden="true" style="font-size: 50%;"></i>
+                                                &nbsp;&nbsp;                                      {{ ucwords(str_replace('_', ' ', $entryType[$i])) }}:
+                                            </td>
+                                            <td style="width:500px;">
+                                                <form method="post" action="{{ url('/user/timeclock/edit') }}">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="id" value="{{ $entry->id or NULL }}" />
+                                                    <input type="hidden" name="type" value="{{ $entryType[$i] }}" />
+                                                    <input type="hidden" name="date" value="{{ $day }}" />
+                                                    <input value="{{ isset($entry->time) ? date('H:i:s', strtotime($entry->time)) : '00:00:00' }}" style="border:none; width:75px;" disabled="disabled" />
+                                                    <input name="time" value="{{ isset($entry->time) ? date('H:i:s', strtotime($entry->time)) : '00:00:00' }}" />
+                                                    <button class="btn btn-block" style="width:150px;display:inline;">Submit Change&nbsp;<i class="fa fa-pencil"></i></button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endfor
 
-                            <tr>
-                                <td style="width:250px;font-weight: 600;">Time Worked: {{ App\Timeclock::getTimeDiff($group) }}</td>
-                            </tr>
-                            </table>
-                        </div>
+                                    <tr>
+                                        <td style="width:250px;font-weight: 600;">Time Worked: {{ App\Timeclock::getTimeDiff($group) }}</td>
+                                    </tr>
+                                </table>
+                            </div>
                         @endforeach
                         <a href="/user" class="btn btn-block">Back</a>
                     </div>
